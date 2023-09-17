c0f99dfb3a164ce2acb9b35cdeab111e2de30a76
NievinEvgeny <zhenya-nievin@mail.ru>
2023-09-18 05:47:57 +0700

    Change const-qualifier to constexpr-qualifier in main

--- src/crypt/main.cpp
+++ src/crypt/main.cpp
@@ -4,16 +4,16 @@
 
 int main()
 {
-    const int64_t base = -37612783631;
-    const int64_t exp = 645813790211;
-    const int64_t mod = 64581;
+    constexpr int64_t base = -37612783631;
+    constexpr int64_t exp = 645813790211;
+    constexpr int64_t mod = 64581;
 
     int64_t result = crypt::pow_mod(base, exp, mod);
 
     std::cout << result << '\n';
 
-    const int64_t first = 3124312425;
-    const int64_t second = 1524345121234;
+    constexpr int64_t first = 3124312425;
+    constexpr int64_t second = 1524345121234;
 
     std::vector<int64_t> res = crypt::extended_gcd(first, second);
 
@@ -23,16 +23,16 @@ int main()
     }
     std::cout << '\n';
 
-    const int64_t private_keyA = 1781234;
-    const int64_t private_keyB = 89102734;
+    constexpr int64_t private_keyA = 1781234;
+    constexpr int64_t private_keyB = 89102734;
 
     int64_t shared_key = crypt::diffie_hellman(private_keyA, private_keyB);
 
     std::cout << "key: " << shared_key << '\n';
 
-    const int64_t base2 = -37612783631;
-    const int64_t answer = -57623;
-    const int64_t mod2 = 64581;
+    constexpr int64_t base2 = -37612783631;
+    constexpr int64_t answer = -57623;
+    constexpr int64_t mod2 = 64581;
 
     int64_t exp_x = crypt::baby_step_giant_step(base2, answer, mod2);
 
